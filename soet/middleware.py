13ed4710524212baaebf684328e29d0e168a85cc
azizzabout <122757964+azizzabout@users.noreply.github.com>
2023-07-17 15:33:27 +0300

    last

--- soet/middleware.py
+++
@@ -1,5 +0,0 @@
-class StackOverflowMiddleware(object):
-    def process_exception(self, request, exception):
-        print exception.__class__.__name__
-        print exception.message
-        return None
